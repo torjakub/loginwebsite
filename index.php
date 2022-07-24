3ef95ac9970ef709efae3f02d02a8c87196d78a0
torjakub <torjakub@gmail.com>
2022-07-24 21:43:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 get_header();
 ?>
-
-<?php the_content(); ?>
-
-<?php
-echo esc_html( get_template_part( 'template-parts/pagination', 'infinite-scroll' ) );
-?>
-
+	<div class="loginpage__Main">
+		<section class="Main__container">
+			<div class="container__text">
+				<h1>Log in</h1>
+				<p class="text">Log in with your data that you received from the photographer</p>
+			</div>
+			<form class="formData">
+				<input class="formData__input" type="text" placeholder="E-mail">
+				<input class="formData__input" type="text" placeholder="Password">
+				<div class="references">
+					<a href="#" class="references__fgpass"> Forgot password?</a>
+					<button class="btn-log">Log in</button>
+				</div>
+			</form>
+		</section>
+	</div>
 <?php
 get_footer();
