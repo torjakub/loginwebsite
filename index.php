6bd0a567c1a3a6e9fa5b3809b4f5a4f4016ed323
torjakub <torjakub@gmail.com>
2022-07-25 22:18:12 +0200

    clear code changes + deleted unnecessary elements

--- index.php
+++ index.php
@@ -2,21 +2,18 @@
 get_header();
 ?>
 	<section class="logpage">
-    	<div class="logpage__background">
-    		<div class="logpage__container">
-    			<h1>Log in</h1>
-    			<p class="logpage__text">Log in with your data that you received from the photographer</p>
-    			<form>
-    				<input class="logpage__input" type="text" placeholder="E-mail">
-    				<input class="logpage__input" type="password" placeholder="Password">
-    				<div class="logpage__submit">
-    						<a href="#" class="logpage__forgot-password"> Forgot password?</a>
-    						<button type="button" class="logpage__button">Log in</button>
-    				</div>
-    			</form>
-    		</div>
+    	<div class="logpage__container">
+    		<h1>Log in</h1>
+    		<p class="logpage__text">Log in with your data that you received from the photographer</p>
+    		<form>
+    			<input class="logpage__input" type="text" placeholder="E-mail">
+    			<input class="logpage__input" type="password" placeholder="Password">
+    			<div class="logpage__submit">
+    				<a href="#" class="logpage__forgot-password"> Forgot password?</a>
+    				<button type="button" class="logpage__button">Log in</button>
+    			</div>
+    		</form>
     	</div>
-
     </section>
 <?php
 get_footer();
